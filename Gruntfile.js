b011b528c7bd4ae88cdf3012cc1f131f9273534b
William Hutter <williamhutter@me.com>
2016-01-12 11:07:21 +0100

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,145 @@
+module.exports = function(grunt) {
+  
+  require('time-grunt')(grunt);
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+  
+    pkg: grunt.file.readJSON('package.json'),
+
+    sass: {
+        dist: {
+          files: {
+            '_site/css/main.css': '_sass/main.scss'
+          }
+        }
+    },
+    svgmin: {
+        dist: {
+            options: {
+                plugins: [
+                    // Don't remove XML declaration (needed to avoid errors creating PNG on Win 7)
+                    { removeXMLProcInst: false }
+                ]
+            },
+            files: [{
+                expand: true,
+                cwd: 'images/',
+                src: ['*.svg'],
+                dest: 'images/svg-optimized/'
+            }]
+        }
+    },
+    grunticon: {
+        myIcons: {
+            files: [{
+                expand: true,
+                cwd: 'images/svg-optimized/',
+                src: ['*.svg', '*.png'],
+                dest: "grunticon/"
+            }],
+            options: {
+                enhanceSVG: true,
+                loadersnippet: "grunticon.loader.js"
+            }
+        }
+    },
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['scripts/*.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n',
+        mangle: false
+      },
+      lib: {
+        src: 'scripts/lib.js',
+        dest: 'scripts/lib.min.js'
+      },
+      main: {
+        src: 'scripts/main.js',
+        dest: 'scripts/main.min.js'
+      }
+    },
+    watch: {
+      options: { livereload: true },
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass'],
+      },
+      html: {
+        files: '**/*.html'
+      },
+      js: {
+        files: '**/main.js'
+      },
+      configFiles: {
+          files: 'Gruntfile.js',
+          options: {
+            reload: true
+          }
+        }
+    },
+    wiredep: {
+      task: {
+        src: [
+          '_includes/head.html',
+          '/styles/styles.scss'
+        ]
+      }
+    },
+    shell: {
+        jekyllBuild: {
+            command: 'jekyll build'
+        },
+        jekyllServe: {
+            command: 'jekyll serve'
+        }
+    },
+    // run tasks in parallel
+    concurrent: {
+        serve: [
+            'watch',
+            'bower_concat',
+            'uglify',
+            'shell:jekyllServe'
+        ],
+        options: {
+            logConcurrentOutput: true
+        }
+    },
+    bower_concat: {
+      all: {
+        dest: 'scripts/lib.js',
+        exclude: [
+          'jquery',
+          'modernizr',
+          'bootstrap-sass'
+        ]
+      }
+    }
+  });
+
+
+  // define the tasks groups
+
+ // Register the grunt serve task
+  grunt.registerTask('serve', [
+      'concurrent:serve'
+  ]);
+
+  // Register the grunt build task
+  grunt.registerTask('build', [
+      'shell:jekyllBuild',
+      'sass'
+  ]);
+  grunt.registerTask('default', ['build']);
+  grunt.registerTask('icons', ['svgmin', 'grunticon:myIcons']);
+
+};
---
+++ scripts/main.js
@@ -0,0 +1,45 @@
+// initiliaze skrollr
+var s = skrollr.init();
+
+jQuery(document).ready(function($){
+	var timelineBlocks = $('.cd-timeline-block'),
+		offset = 0.8;
+
+	//hide timeline blocks which are outside the viewport
+	hideBlocks(timelineBlocks, offset);
+
+	//on scolling, show/animate timeline blocks when enter the viewport
+	$(window).on('scroll', function(){
+		(!window.requestAnimationFrame) 
+			? setTimeout(function(){ showBlocks(timelineBlocks, offset); }, 100)
+			: window.requestAnimationFrame(function(){ showBlocks(timelineBlocks, offset); });
+	});
+
+	function hideBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top > $(window).scrollTop()+$(window).height()*offset ) && $(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+		});
+	}
+
+	function showBlocks(blocks, offset) {
+		blocks.each(function(){
+			( $(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) && $(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+		});
+	}
+
+
+	$('a').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $( $.attr(this, 'href') ).offset().top
+	    }, 500);
+	    return false;
+	});
+
+	$('.numbers__counter .numbers__num').counterUp({
+	    delay: 10, // the delay time in ms
+	    time: 1000 // the speed time in ms
+	  });
+
+
+});
+
